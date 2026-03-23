fe182fd11413caebdf1ea5e90636e2465f048e20
Borhan Torkfar <Borhan@120degreez.net>
2026-03-23 13:33:54 -0700

    Refine iPhone layout and compact project list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "120-degreez-pwa-cache-v1";
+const CACHE_NAME = "120-degreez-pwa-cache-v2";
 const APP_ASSETS = [
   "./",
   "./index.html",
@@ -9,14 +9,18 @@ const APP_ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-    ))
+    Promise.all([
+      caches.keys().then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )),
+      self.clients.claim(),
+    ])
   );
 });
 
